8ae5d247a70084033109a8f86a58a0ae227c202d
NEO <qj82189@gmail.com>
2023-08-17 14:51:52 -0400

    Added technology badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
 
-  if ( license != null) {
+  if (license != null) {
 
     var badgeLink = `![GitHub](https://img.shields.io/github/license/TheQuentinJones/${license.fileName})`
 
@@ -15,24 +15,80 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderTechBadge(license) {
+
+  var techBadge = []
+
+  license.technologies.forEach(element => {
+
+    switch (element) {
+
+      case 'React':
+        techBadge.push("[![REACT Shield](https://img.shields.io/badge/React-222222?&style=for-the-badge&logo=react)](https://reactjs.org/)")
+        break
+      case 'GraphQL':
+        techBadge.push("[![GraphQl Shield](https://img.shields.io/badge/GraphQl-E10098?&style=for-the-badge&logo=graphql&logoColor=white)](https://graphql.org/)")
+        break
+      case 'Mongoose':
+        techBadge.push("[![Mongoose Shield](https://img.shields.io/badge/Mongoose-AA2929?&style=for-the-badge&logo=matrix&logoColor=white)](https://mongoosejs.com/)")
+      case 'MongoDB':
+        techBadge.push("[![MongoDB Shield](https://img.shields.io/badge/MongoDB-47A248?&style=for-the-badge&logo=mongodb&logoColor=white)](https://www.mongodb.com/)")
+        break
+      case 'Express':
+        techBadge.push("[![Express Shield](https://img.shields.io/badge/Express-000000?&style=for-the-badge&logo=express&logoColor=white)](http://expressjs.com/)")
+        break
+      case 'Heroku':
+        techBadge.push("[![Heroku Shield](https://img.shields.io/badge/Heroku-430098?&style=for-the-badge&logo=heroku&logoColor=white)](https://www.heroku.com/what)")
+        break
+      case 'Node':
+        techBadge.push("[![Node.js Shield](https://img.shields.io/badge/Node.js-339933?&style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/en/)")
+        break
+      case 'NPM':
+        techBadge.push("[![NPM Shield](https://img.shields.io/badge/NPM-333333?&style=for-the-badge&logo=npm&logoColor=white)](https://www.npmjs.com/)")
+        break
+      case 'JavaScript':
+        techBadge.push("[![JavaScript Shield](https://img.shields.io/badge/JavaScript-F7DF1E?&style=for-the-badge&logo=javascript&logoColor=272727)](https://developer.mozilla.org/en-US/docs/Web/JavaScript)")
+      case 'HTML':
+        techBadge.push("[![HTML Shield](https://img.shields.io/badge/HTML5-E34F26?&style=for-the-badge&logo=html5&logoColor=white)](https://developer.mozilla.org/en-US/docs/Glossary/HTML5)")
+        break
+      case 'CSS':
+        techBadge.push("[![CSS Shield](https://img.shields.io/badge/CSS-1572B6?&style=for-the-badge&logo=css3&logoColor=white)](https://developer.mozilla.org/en-US/docs/Web/CSS)")
+        break
+      case 'Bootstrap':
+        techBadge.push("[![Bootstrap CSS Shield](https://img.shields.io/badge/Bootstrap_CSS-7952B3?&style=for-the-badge&logo=bootstrap&logoColor=white)](https://getbootstrap.com/)")
+        break
+    }   
+
+  });
+
+  return techBadge.join(" ")
+
+
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   // console.log(data, 'this is in the generate markdown file')
 
-  if ( data.licenses != null ) {
+  if (data.licenses != null) {
 
     var badgeLink = renderLicenseBadge(data)
 
   }
 
+  if (data.technologies != null) {
 
-  
+    var techBadgeRendering = renderTechBadge(data)
+
+
+  }
+
+
+
   return `# <h1 align="center">${data.fileName}</h1>
 
   ${badgeLink}
@@ -40,6 +96,10 @@ function generateMarkdown(data) {
   ## Description
           
   ${data.description}
+
+  ## Built With:
+
+  ${techBadgeRendering}
          
   ## Table of Contents
 
@@ -83,7 +143,7 @@ function generateMarkdown(data) {
   
   `
 
-  
+
 }
 
 module.exports = generateMarkdown;
